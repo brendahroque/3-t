ecae9a585e73ae960b1f396c4d3b51e33abed1d8
brendahroque <129418439+brendahroque@users.noreply.github.com>
2023-09-21 14:35:23 -0300

    Add files via upload

---
+++ 3trimestre-main/main.js
@@ -0,0 +1,18 @@
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < ListaDeTeclas.length) {
+    ListaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador +1;
+
+    console.log(contador);
+
+}
